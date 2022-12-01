b003bdbf10772ccb68df7e2853f92ef2a0c7bb36
Guang Liang <guang.liang@mthreads.com>
2022-12-01 17:33:55 +0800

    Add: tests/test_prosody_predictor.py

--- examples/chinese_prosody_polyphone/tests/test_prosody_predictor.py
+++ examples/chinese_prosody_polyphone/tests/test_prosody_predictor.py
@@ -1,12 +1,15 @@
 import pytest
 
-from _my import rhy_with_tn as psp
+from _my import prosody_predictor as psp
 
 
 @pytest.fixture(scope='module')
 def psp_model():
-    # yield psp.ProsodyPredictor('local', 'exp/9.onnx')
-    yield psp.ProsodyPredictor('local', '_backup/exp_v21/9.onnx')
+    config = {
+        'pretrain_bert_model': '/nfs1/nlp/models/MTBert_v0.0.3',
+        'model_path': '/nfs2/speech/model/tts/frontend/wetts_psp/model/exp_v21/9.onnx',
+    }
+    yield psp.ProsodyPredictor(config=config)
 
 
 @pytest.mark.parametrize('text,expect,use_sp', [
@@ -21,42 +24,26 @@ def psp_model():
         True,
     ),
     (
-        '唐剑说：“从前端算法、云平台、语音模组、自然语言理解算法 #2 等 #3 都是我们自研的。”',
+        '唐剑说：“从前端算法、云平台、语音模组、自然语言理解算法等都是我们自研的。”',
         '唐剑说：“从前端算法、云平台、语音模组、自然语言理解算法等 #3 都是我们自研的。”',
         False,
     ),
     (
-        '唐剑说：“从前端算法、云平台、语音模组、自然语言理解算法 #2 等 #3 都是我们自研的。”',
+        '唐剑说：“从前端算法、云平台、语音模组、自然语言理解算法等都是我们自研的。”',
         '唐剑说：“从前端算法、云平台、语音模组、自然语言理解算法等 sp1 都是我们自研的。”',
         True,
     ),
     (
         '商业保险将合并您的工资薪金计算缴纳个人所得税。',
         '商业保险将合并您的工资薪金 #2 计算缴纳个人所得税。',
-		False,
+        False,
     ),
     (
-            '法务 #2 制定的或审核过的订单模板 #2 不需要审核',
-            '法务制定的或审核过的订单模板不需要审核',
-            False,
+        '法务制定的或审核过的订单模板不需要审核',
+        '法务制定的或审核过的订单模板不需要审核',
+        False,
     )
 ])
 def test_prosody_predictor(text, expect, use_sp, psp_model):
     assert psp_model.predict(text, use_sp=use_sp) == expect
 
-
-@pytest.mark.parametrize('text_len', [
-    5,
-    10,
-    20,
-    30,
-    50,
-    100,
-    150,
-    200,
-])
-def test_benchmark(text_len, benchmark, psp_model):
-    import faker
-    fake = faker.Faker('zh_CN')
-    text = fake.sentence(nb_words=text_len, variable_nb_words=True)[:text_len]
-    benchmark(psp_model.predict, text, use_sp=True)
