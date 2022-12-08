b128d0f29779a1c7a36404bdacadadfa19137bad
Guang Liang <guang.liang@mthreads.com>
2022-12-08 15:58:19 +0800

    Update: support adding rank in text

--- examples/chinese_prosody_polyphone/_my/prosody_predictor.py
+++ examples/chinese_prosody_polyphone/_my/prosody_predictor.py
@@ -14,7 +14,6 @@ except ImportError:
     print('Please install onnxruntime!')
     sys.exit(1)
 
-
 from . import rhy_postprocess
 
 
@@ -28,9 +27,9 @@ class FrontendModel(nn.Module):
         d_model = self.bert.config.to_dict()['hidden_size']
         assert d_model in (768, 1024), 'Expected bert encoder input dim is 768 or 1024'
         self.transform = nn.TransformerEncoderLayer(d_model=d_model,
-                                                    nhead=8,
-                                                    dim_feedforward=2048,
-                                                    batch_first=True)
+                nhead=8,
+                dim_feedforward=2048,
+                batch_first=True)
         self.phone_classifier = nn.Linear(d_model, num_phones)
         self.prosody_classifier = nn.Linear(d_model, num_prosody)
 
@@ -38,7 +37,7 @@ class FrontendModel(nn.Module):
         mask = x['attention_mask'] == 0
         bert_output = self.bert(**x)
         x = self.transform(bert_output.last_hidden_state,
-                           src_key_padding_mask=mask)
+                src_key_padding_mask=mask)
         phone_pred = self.phone_classifier(x)
         prosody_pred = self.prosody_classifier(x)
         return phone_pred, prosody_pred
@@ -49,10 +48,10 @@ class FrontendModel(nn.Module):
     def export_forward(self, x):
         assert x.size(0) == 1
         x = {
-            'input_ids': x,
-            'token_type_ids': torch.zeros(1, x.size(1), dtype=torch.int64).to(x.device),
-            'attention_mask': torch.ones(1, x.size(1), dtype=torch.int64).to(x.device)
-        }
+                'input_ids': x,
+                'token_type_ids': torch.zeros(1, x.size(1), dtype=torch.int64).to(x.device),
+                'attention_mask': torch.ones(1, x.size(1), dtype=torch.int64).to(x.device)
+                }
         phone_logits, prosody_logits = self._forward(x)
         phone_pred = F.softmax(phone_logits, dim=-1)
         prosody_pred = F.softmax(prosody_logits, dim=-1)
@@ -70,7 +69,48 @@ def _add_prosody_into_text(text, prosody):
             # lst.append('sp' + str(rhy))
             lst.append(f' #{rhy} ')
     return lst
-    
+
+
+def _remove_prosody(text, use_sp=False):
+    if use_sp:
+        pattern = r' (sp[0-4]) '
+    else:
+        pattern = r' (#[0-4]) '
+    lst = re.sub(pattern, r'_\1_', text).split('_')
+
+    rank_lst = []
+    char_lst = []
+    preffix_sum = 0
+    for x in lst:
+        if re.match('(sp|#)[0-4]', x):
+            rank_lst.append((preffix_sum, x))
+        else:
+            char_lst.append(x)
+            preffix_sum += len(x)
+    return rank_lst, ''.join(char_lst)
+
+
+def _combine_prosody(text, rank_lst, pred_rank_lst, min_dist=4):
+    d = dict(pred_rank_lst)
+    logging.info(f'pred prosody dict: {d}')
+    logging.info(f'artifact prosody list: {rank_lst}')
+    for pos, _ in rank_lst:
+        for i in range(max(0, pos - min_dist), pos + min_dist):
+            if i in d:
+                logging.info(f'ignore prosody nearby artifact pos {pos}: {i} {d[i]}')
+                del d[i]
+    for pos, rank in rank_lst:
+        if rank in d:
+            logging.info(f'use artifact over pred: {rank} => {d[pos]}')
+        else:
+            logging.info(f'add artifact rank: pos={pos} rank={rank}')
+        d[pos] = rank
+
+    char_lst = list(text)
+    for pos in sorted(d, reverse=True):
+        char_lst.insert(pos, f' {d[pos]} ')
+        # logging.debug(f'char_lst: {char_lst}')
+    return ''.join(char_lst)
 
 
 def _predict_zh_prosody(frontend, text, use_pinyin=False):
@@ -95,7 +135,7 @@ def _predict_prosody(frontend, text, use_pinyin=False):
         segment_lst = re.sub(rf'([{sentence_sep}]+)', r'_\1_', text).split('_')
         return segment_lst
 
-    sentence_sep = r'\Wa-zA-Z\s'
+    sentence_sep = r'\Wa-zA-Z\s'  # 区分中英文
     segment_lst = _split_sentence(text)
     if len(segment_lst) > 1:
         logging.info('split sentence segment list: %s', segment_lst)
@@ -126,7 +166,7 @@ class FrontendPtRuntime(object):
             num_phones: int = 876,  # local/polyphone_phone.txt
             num_prosody: int = 5,   # local/prosody2id.txt
             device: str = 'cpu',
-    ):
+            ):
         # Init model
         model = FrontendModel(num_phones, num_prosody, pretrain_bert_model)
 
@@ -144,11 +184,11 @@ class FrontendPtRuntime(object):
         with torch.no_grad():
             # tokens = self.tokenizer.encode(x, add_special_tokens=False)
             inputs = self.tokenizer(list(x),
-                             padding=True,
-                             truncation=True,
-                             is_split_into_words=True,
-                             return_tensors="pt"
-                         )['input_ids']
+                    padding=True,
+                    truncation=True,
+                    is_split_into_words=True,
+                    return_tensors="pt"
+                    )['input_ids']
             inputs = inputs.to(self.device)
             phone_pred, prosody_pred = self.model.export_forward(inputs)
         return phone_pred, prosody_pred
@@ -160,25 +200,25 @@ class FrontendOnnxRuntime(object):
             polyphone_prosody_model: str,
             pretrain_bert_model: str,
             device: str = 'cpu',
-    ):
+            ):
         provider_map = {
-            'cpu': 'CPUExecutionProvider',
-            'cuda': 'CUDAExecutionProvider',
-            'tensorrt': 'TensorrtExecutionProvider',
-            # 'mtgpu': 'xxx',
-        }
+                'cpu': 'CPUExecutionProvider',
+                'cuda': 'CUDAExecutionProvider',
+                'tensorrt': 'TensorrtExecutionProvider',
+                # 'mtgpu': 'xxx',
+                }
         self.ppm_sess = ort.InferenceSession(
-            polyphone_prosody_model,
-            providers=[
-                provider_map[device],
-                ]
-            )
+                polyphone_prosody_model,
+                providers=[
+                    provider_map[device],
+                    ]
+                )
         self.tokenizer = AutoTokenizer.from_pretrained(pretrain_bert_model)
 
     def run(self, x):
         tokens = self.tokenizer(list(x),
-                           is_split_into_words=True,
-                           return_tensors="np")['input_ids']
+                is_split_into_words=True,
+                return_tensors="np")['input_ids']
         ort_inputs = {'input': tokens}
         phone_pred, prosody_pred = self.ppm_sess.run(None, ort_inputs)
         return phone_pred, prosody_pred
@@ -187,19 +227,20 @@ class FrontendOnnxRuntime(object):
 class Frontend(object):
 
     def __init__(
-        self,
-        polyphone_prosody_model: str,
-        pretrain_bert_model: str,
-        # hanzi2pinyin_file: str,
-        # trad2simple_file: str,
-        # polyphone_phone_file: str,
-        # polyphone_character_file: str,
-    ):
+            self,
+            polyphone_prosody_model: str,
+            pretrain_bert_model: str,
+            # hanzi2pinyin_file: str,
+            # trad2simple_file: str,
+            # polyphone_phone_file: str,
+            # polyphone_character_file: str,
+            device: str = 'cpu',
+            ):
         runtime_map = {
-            'onnx': FrontendOnnxRuntime,
-            'pt': FrontendPtRuntime,
-            'pth': FrontendPtRuntime,
-        }
+                'onnx': FrontendOnnxRuntime,
+                'pt': FrontendPtRuntime,
+                'pth': FrontendPtRuntime,
+                }
         model_extension = polyphone_prosody_model.rsplit('.', 1)[-1]
         assert model_extension in runtime_map, \
                 f'model extension must be {list(runtime_map)}, but got {model_extension}'
@@ -207,8 +248,7 @@ class Frontend(object):
         runtime_cls = runtime_map[model_extension]
         logging.info(f'use runtime {runtime_cls}')
 
-        self.sess = runtime_cls(polyphone_prosody_model, pretrain_bert_model, device='cpu')
-        # self.sess = runtime_cls(polyphone_prosody_model, pretrain_bert_model, device='cuda')
+        self.sess = runtime_cls(polyphone_prosody_model, pretrain_bert_model, device=device)
 
         # self.hanzi2pinyin = Hanzi2Pinyin(hanzi2pinyin_file)
         # self.tn = Normalizer()
@@ -246,27 +286,36 @@ class Frontend(object):
 class ProsodyPredictor:
 
     _DEFAULT_CONFIG = {
-        'pretrain_bert_model': '/nfs1/nlp/models/MTBert_v0.0.3',
-        'model_path': '/nfs2/speech/model/tts/frontend/wetts_psp/model/exp_v21/9.onnx',
-        # 'model_path': '/nfs2/speech/model/tts/frontend/wetts_psp/model/exp_v21/9.pt',
-    }
+            'pretrain_bert_model': '/nfs1/nlp/models/MTBert_v0.0.3',
+            # 'model_path': '/nfs2/speech/model/tts/frontend/wetts_psp/model/exp_v21/9.onnx',
+            'model_path': '/nfs2/speech/model/tts/frontend/wetts_psp/model/exp_v21/9.pt',
+            }
 
     def __init__(self, config: dict = None):
         if config:
             assert isinstance(config, dict), "config type must be dict"
         else:
             config = ProsodyPredictor._DEFAULT_CONFIG
-            
-        self.frontend = Frontend(config['model_path'], config['pretrain_bert_model'])
 
-    def predict(self, text: str, use_sp: bool=False, use_postprocess=True) -> str:
+        self.frontend = Frontend(
+                config['model_path'],
+                config['pretrain_bert_model'],
+                device=config.get('device', 'cpu')
+        )
+        return
+
+    def predict(self, text: str, use_sp: bool=False, use_postprocess=True, use_artifact=False) -> str:
         """
         摩尔线程真棒 => 摩尔线程 sp0 真棒
 
         Args:
             @use_sp: with `sp0/sp1` if `use_sp=True`; otherwise `#2/#3` by default.
+            @use_postprocess: apply postprocess rules to adjust prosody.
+            @use_artifact: parse prosody `sp0/sp1` or `#2/#3` in text added by human.
         """
         # text is hanzi
+        if use_artifact:
+            rank_lst, text = _remove_prosody(text, use_sp=use_sp)
 
         # pinyin, prosody, hanzi = self.frontend.g2p(text)
         # seg_lst = _add_prosody_into_text(text, prosody)
@@ -276,12 +325,18 @@ class ProsodyPredictor:
 
         if use_postprocess:
             text = rhy_postprocess.process_text(text)
+
         if use_sp:
             text = text.replace('#2', 'sp0').replace('#3', 'sp1')
+
+        if use_artifact and rank_lst:
+            pred_rank_lst, text = _remove_prosody(text, use_sp=use_sp)
+            text = _combine_prosody(text, rank_lst, pred_rank_lst)
+
         return text
 
 
 if __name__ == '__main__':
-	import fire
-	fire.Fire(ProsodyPredictor)
+    import fire
+    fire.Fire(ProsodyPredictor)
 
